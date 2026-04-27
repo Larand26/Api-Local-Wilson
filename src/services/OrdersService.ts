93f39303285b808b2333bec5c492d13bf5a1179e
Victor de Brito Laranjeira <vbl09012003vbl@gmail.com>
2026-04-27 13:23:01 -0300

    fix: resolve possível gargalo no OrdersService

--- src/services/OrdersService.ts
+++ src/services/OrdersService.ts
@@ -1,16 +1,22 @@
 import fs from "fs";
-import path from "path";
+import path, { dirname } from "path";
 
 import SqlServer from "../db/SqlServer.js";
 
 import type { Iresponse } from "../interface/interfaces.js";
 
+const __dirname = dirname(import.meta.url);
+
 abstract class OrdersService {
+  private static queryPath = path.join(__dirname, "../queries/getOrders.sql");
+  private static query = fs.readFileSync(this.queryPath, "utf8");
+
   static async getOrders(): Promise<Iresponse> {
     try {
-      const queryPath = path.join("src", "db", "query", "getOrdersQuery.sql");
-      const query = fs.readFileSync(queryPath, "utf8");
-      const orders = await SqlServer.query(query);
+      if (!this.query) {
+        throw new Error("SQL query not found");
+      }
+      const orders = await SqlServer.query(this.query);
       return { success: true, data: orders };
     } catch (error) {
       console.error("Error fetching orders:", error);
